511f081581b3af575049865a7f1e2770f42cbbd3
Chih-Hao Tsai <a968574123@hotmail.com>
2016-09-11 22:08:03 +0800

    Release v1.0.1

--- args.js
+++ args.js
@@ -2,7 +2,6 @@ const program = require('commander');
 const path = require('path');
 
 program
-    .version('0.0.1')
     .option('-c, --config [path]', 'set a config file. defaults to ./config.js', './config.js')
     .parse(process.argv);
 
