37df2961998293ed3eaf18abd1827f3fcfac850f
Dimo <a968574123@hotmail.com>
2016-09-24 06:01:35 +0800

    Replace package node-telegram-bot-api with node-telegram-bot-api-latest

--- telegram_bot.js
+++ telegram_bot.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api-latest');
 module.exports = class TelegramBot_ extends TelegramBot {
     constructor(config) {
         super(config.telegramBotToken, { polling: false });
