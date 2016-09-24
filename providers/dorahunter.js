765dc76240a0f2b736460ff83f0d375c66c56e7a
Dimo <a968574123@hotmail.com>
2016-09-24 21:03:23 +0800

    Support showing IVs and Moves on dora

--- providers/dorahunter.js
+++ providers/dorahunter.js
@@ -7,15 +7,16 @@ const uuid = require('uuid');
 const debug = require('debug')('provider:dorahunter');
 const Provider = require('./provider.js');
 const pokemonNames = require('../pokemon_names.js');
+const pokemonMoves = require('../pokemon_moves.js');
 
 module.exports = class DoraHunter extends Provider {
     constructor(config) {
         super(config);
-        this._deviceId = uuid.v4();
-        this._url = 'http://go.poedb.tw/pmgo/raw.php';
+        this._deviceId = '68042f1c-5ad6-4df7-81cb-dadfcbde5b19';
+        this._url = 'http://go.poedb.tw/pmgo/rawc.php';
         this._filteredPokemonIds = config.filteredPokemonIds ? config.filteredPokemonIds.sort((a,b) => a-b) : null;
-        this._iv = Buffer.from([77, 90, 80, 0, 2, 0, 122, 9, 4, 0, 15, 127, 95, 47, 3, 6]);
-        this._key = Buffer.from([82, 97, 114, 33, 26, 7, 0, 63, 32, 115, 2, 31, 13, 122, 119, 68, 51, 0, 2, 0, 121, 108, 116, 32, 2, 63, 0, 23, 97, 0, 0, 62]);
+        this._iv = Buffer.from([101, 103, 126, 79, 53, 71, 29, 124, 32, 49, 118, 78, 37, 47, 54, 108]);
+        this._key = Buffer.from([83, 89, 69, 59, 11, 57, 61, 38, 23, 111, 37, 45, 108, 114, 9, 83, 127, 93, 83, 74, 112, 101, 56, 36, 47, 125, 108, 101, 31, 22, 120, 100]);
         this._algorithm = 'AES-256-CBC';
     }
 
@@ -69,6 +70,8 @@ module.exports = class DoraHunter extends Provider {
             let entry = {};
             let end = entry_.t * 1000;
             let diff = moment(end).diff(moment());
+            let iv = _.isArray(entry_.iv) ? entry_.iv.map(parseFloat) : [];
+            let move = _.isArray(entry_.m) ? entry_.m.map(parseFloat) : [];
             entry.latitude = entry_.a;
             entry.longitude = entry_.o;
             entry.pokemonId = entry_.i;
@@ -77,6 +80,12 @@ module.exports = class DoraHunter extends Provider {
             entry.until = moment(end);
             entry.direction = 'https://www.google.com/maps/dir/Current+Location/' + entry_.a + ',' + entry_.o;
             entry.uniqueId = `${entry_.i}-${entry_.t}`;
+            entry.individualAttack =  iv[0];
+            entry.individualDefense = iv[1];
+            entry.individualStamina = iv[2];
+            entry.IVPerfection = Math.floor(((entry.individualAttack + entry.individualDefense + entry.individualStamina) / 45) * 100);
+            entry.move1 = pokemonMoves[move[0]];
+            entry.move2 = pokemonMoves[move[1]];
             return entry;
         });
         return processed;
