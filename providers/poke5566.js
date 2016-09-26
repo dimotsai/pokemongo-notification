4741bb739287b739c0d09ec0d67f44eaa2d0f18a
Dimo <a968574123@hotmail.com>
2016-09-26 13:28:01 +0800

    Update GA timestamp to prevent expiration

--- providers/poke5566.js
+++ providers/poke5566.js
@@ -35,7 +35,7 @@ class Poke5566 extends Provider {
             headers: {
                 'X-Requested-With': 'XMLHttpRequest',
                 'Referer': 'https://poke5566.com/',
-                'Cookie': 'ss=poke5566; iv=0; star=1; _ga=GA1.2.645874393.1474666335;',
+                'Cookie': `ss=poke5566; iv=0; star=1; _ga=GA1.2.645874393.${moment().unix()};`,
                 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
             }
         };
